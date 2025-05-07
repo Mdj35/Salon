14cf423eece9b917c7906fc04fb76ddfadefeb6b
Mdj35 <162547760+Mdj35@users.noreply.github.com>
2025-05-07 09:50:25 +0000

    sample

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import Services from "./Services";
+import Appointment from "./Appointments";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<BrowserRouter>
 		<App />
+		<Services />
+		<Appointment />
 	</BrowserRouter>,
 );
